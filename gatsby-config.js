764d06b61c45dd3def8356c42ed4617c4d1d6dac
seekerpig <laserklet@gmail.com>
2021-05-07 22:15:32 +0800

    added google tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,31 +1,42 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
+})
+
 module.exports = {
-  siteMetadata: {
-    description: "Portfolio of Soh Zu Wei",
-    locale: "en",
-    title: "Soh Zu Wei | Portfolio",
-    formspreeEndpoint: "https://formspree.io/f/mnqlqklk",
-  },
-  plugins: [
-    {
-      resolve: "@wkocjan/gatsby-theme-intro",
-      options: {
-        basePath: "/",
-        contentPath: "content/",
-        showThemeLogo: false,
-        theme: "classic",
-      },
+    siteMetadata: {
+        description: "Portfolio of Soh Zu Wei",
+        locale: "en",
+        title: "Soh Zu Wei | Portfolio",
+        formspreeEndpoint: "https://formspree.io/f/mnqlqklk",
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
-      },
-    },
-  ],
-}
+    plugins: [
+        {
+            resolve: "@wkocjan/gatsby-theme-intro",
+            options: {
+                basePath: "/",
+                contentPath: "content/",
+                showThemeLogo: false,
+                theme: "classic",
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                // The property ID; the tracking code won't be generated without it
+                trackingId: process.env.GA_TRACKING_ID,
+            },
+        },
+    ],
+};
