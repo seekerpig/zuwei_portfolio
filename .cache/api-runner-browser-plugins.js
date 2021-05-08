a090ce06c816ae2b3d31ca52e27e1076a217c602
seekerpig <laserklet@gmail.com>
2021-05-08 22:44:59 +0800

    made final changes.

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,10 +1,10 @@
 module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-google-gtag/gatsby-browser.js'),
+      options: {"plugins":[],"trackingIds":["G-6KHSKG1D6F"],"gtagConfig":{"optimize_id":"OPT_CONTAINER_ID","anonymize_ip":true,"cookie_expires":0},"pluginConfig":{"head":false,"respectDNT":true,"exclude":["/preview/**","/do-not-track/me/too/"]}},
+    },{
       plugin: require('../node_modules/@wkocjan/gatsby-theme-intro/node_modules/gatsby-plugin-image/gatsby-browser.js'),
       options: {"plugins":[]},
     },{
       plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
       options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/favicon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"ace5489fc3177061b86f436d132c6d56"},
-    },{
-      plugin: require('../../../node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
-      options: {"plugins":[],"trackingId":"G-6KHSKG1D6F","head":false,"anonymize":false,"respectDNT":false,"exclude":[],"pageTransitionDelay":0},
     }]
--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,4 +1,8 @@
 var plugins = [{
+      name: 'gatsby-plugin-google-gtag',
+      plugin: require('C:/Users/zw/OneDrive/Desktop/zuwei_portfolio/gatsby-theme-intro/node_modules/gatsby-plugin-google-gtag/gatsby-ssr'),
+      options: {"plugins":[],"trackingIds":["G-6KHSKG1D6F"],"gtagConfig":{"optimize_id":"OPT_CONTAINER_ID","anonymize_ip":true,"cookie_expires":0},"pluginConfig":{"head":false,"respectDNT":true,"exclude":["/preview/**","/do-not-track/me/too/"]}},
+    },{
       name: 'gatsby-plugin-react-helmet',
       plugin: require('C:/Users/zw/OneDrive/Desktop/zuwei_portfolio/gatsby-theme-intro/node_modules/@wkocjan/gatsby-theme-intro/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
@@ -10,10 +14,6 @@ var plugins = [{
       name: 'gatsby-plugin-manifest',
       plugin: require('C:/Users/zw/OneDrive/Desktop/zuwei_portfolio/gatsby-theme-intro/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/favicon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"ace5489fc3177061b86f436d132c6d56"},
-    },{
-      name: 'gatsby-plugin-google-analytics',
-      plugin: require('C:/Users/zw/OneDrive/Desktop/node_modules/gatsby-plugin-google-analytics/gatsby-ssr'),
-      options: {"plugins":[],"trackingId":"G-6KHSKG1D6F","head":false,"anonymize":false,"respectDNT":false,"exclude":[],"pageTransitionDelay":0},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
